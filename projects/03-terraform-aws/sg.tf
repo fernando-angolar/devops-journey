680d87c15ac4845a4ccad87d732603b4ab1d342f
Fernando Angolar <fernandoangolar581@gmail.com>
2026-09-21 20:03:43 +0100

    feat: Security Group com Terraform

---
+++ projects/03-terraform-aws/sg.tf
@@ -0,0 +1,49 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "web" {
+  name        = "devops-journey-tf-sg"
+  description = "SG da jornada DevOps (Terraform)"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "devops-journey-tf-sg"
+    Project = "devops-journey"
+  }
+}
+
+output "security_group_id" {
+  value = aws_security_group.web.id
+}
