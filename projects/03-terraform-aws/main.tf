65470dbac3ab029c8e098bdbd92525a2f89477bb
Fernando Angolar <fernandoangolar581@gmail.com>
2026-09-21 19:41:12 +0100

    feat: provider AWS e data sources no Terraform

---
+++ projects/03-terraform-aws/main.tf
@@ -0,0 +1,15 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+output "account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "region" {
+  value = data.aws_region.current.name
+}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.current.arn
+}
